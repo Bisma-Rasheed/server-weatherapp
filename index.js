128798fe54798f98a82dae473d37abcc68dd9528
Bisma-Rasheed <bismarasheed95@gmail.com>
2023-01-25 22:16:05 +0500

    updated

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const io = new Server(server, {
         // credentials: true
     },
 });
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+  });
 const dotenv = require('dotenv');
 const routes = require('./controller/routesController');
 const cors = require('cors');
